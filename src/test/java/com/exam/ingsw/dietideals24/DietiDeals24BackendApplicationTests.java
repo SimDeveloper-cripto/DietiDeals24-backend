25cdb0190982823262650ab5969ad5b1e13b1c50
SimDeveloper-cripto <catapano.smn.2001@gmail.com>
2023-11-18 14:34:38 +0100

    First commit, added project's first dependencies

---
+++ src/test/java/com/exam/ingsw/dietideals24/DietiDeals24BackendApplicationTests.java
@@ -0,0 +1,11 @@
+
+package com.exam.ingsw.dietideals24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietiDeals24BackendApplicationTests {
+	@Test
+	void contextLoads() {}
+}
