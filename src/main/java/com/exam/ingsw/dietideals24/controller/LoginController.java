b970c8bad130a0a139d4662b8c12d4e9410a83ae
SimDeveloper-cripto <catapano.smn.2001@gmail.com>
2023-12-15 15:22:44 +0100

    Login is now a Get Request.

--- src/main/java/com/exam/ingsw/dietideals24/controller/LoginController.java
+++ src/main/java/com/exam/ingsw/dietideals24/controller/LoginController.java
@@ -2,10 +2,10 @@ package com.exam.ingsw.dietideals24.controller;
 
 import com.exam.ingsw.dietideals24.model.User;
 import com.exam.ingsw.dietideals24.service.IUserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,8 +14,11 @@ public class LoginController {
     @Qualifier("UserService")
     private IUserService userService;
 
-    @PostMapping("/userLogin")
-    public User findUser(@RequestBody User user) {
+    @GetMapping("/userLogin")
+    public User findUser(@RequestParam String email, @RequestParam String password) {
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
         return userService.loginUser(user);
     }
 }
