4883e66f07b2c7e0a36c279ecf03ebcdd41f9672
SimDeveloper-cripto <catapano.smn.2001@gmail.com>
2024-01-14 11:33:59 +0100

    Followed SonarQube suggestions to clean and optimize the current code pt4

--- src/test/java/com/exam/ingsw/dietideals24/DietiDeals24BackendApplicationTests.java
+++ src/test/java/com/exam/ingsw/dietideals24/DietiDeals24BackendApplicationTests.java
@@ -2,18 +2,9 @@ package com.exam.ingsw.dietideals24;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.context.ApplicationContext;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class DietiDeals24BackendApplicationTests {
-	@Autowired
-	private ApplicationContext applicationContext;
-
 	@Test
-	void contextLoads() {
-		assertNotNull(applicationContext, "The application context should not be null!");
-	}
+	void contextLoads() {}
 }
